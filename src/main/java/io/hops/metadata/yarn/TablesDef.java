830dd046749b888fc038374aa413c8a5138c276c
gautier <gautier@sics.se>
2015-08-09 11:10:05 +0200

    small corrections

--- src/main/java/io/hops/metadata/yarn/TablesDef.java
+++ src/main/java/io/hops/metadata/yarn/TablesDef.java
@@ -99,7 +99,7 @@ public class TablesDef {
   public static interface JustLaunchedContainersTableDef {
 
     public static final String TABLE_NAME = "yarn_justlaunchedcontainers";
-    public static final String CONTAINERID = "containerid_id";
+    public static final String CONTAINERID = "containerid";
     public static final String RMNODEID = "rmnodeid";
   }
 
